c55e9b0b0ea06f22eb59ff35957d1fa5544740c5
Igor <igor.torquato.20@gmail.com>
2024-12-05 19:23:31 -0300

    add: exercise 1 solved

--- 11- Javascript DOM/exercicio_001/app.js
+++ 11- Javascript DOM/exercicio_001/app.js
@@ -8,3 +8,18 @@ Enunciado: CONTADOR
     um dos botões, o utilizador irá aumentar ou diminuir o valor em uma unidade.
 
 ---------------------------------------------------------------------------- */
+const number_element = document.getElementById("valor")
+const decrement_btn = document.getElementById("btn_decremento")
+const increment_btn = document.getElementById("btn_incremento")
+
+decrement_btn.addEventListener('click', (e)=>{
+    let number_value = parseInt(number_element.textContent)
+    number_value= number_value-1
+    number_element.textContent = number_value
+})
+
+increment_btn.addEventListener('click', (e)=>{
+    let number_value = parseInt(number_element.textContent)
+    number_value= number_value+1
+    number_element.textContent = number_value
+})
