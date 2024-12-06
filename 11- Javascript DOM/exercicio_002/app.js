53b2b7df47a07f5302a22b8daea8c489b3d90c37
Igor <igor.torquato.20@gmail.com>
2024-12-05 21:29:20 -0300

    add: exercise 2 solved

--- 11- Javascript DOM/exercicio_002/app.js
+++ 11- Javascript DOM/exercicio_002/app.js
@@ -8,3 +8,51 @@ Enunciado: CONTADOR COM INTERVALO DE VALORES
     O valor nunca poderá ser inferior a -10 ou superior a 10.
 
 ---------------------------------------------------------------------------- */
+const value_element = document.getElementById('valor')
+const decrement_btn = document.getElementById('btn_decremento')
+const increment_btn = document.getElementById('btn_incremento')
+const container = document.getElementsByClassName('container mt-5')
+
+let number = parseInt(value_element.textContent)
+
+decrement_btn.addEventListener('click',(e)=>{
+    if(number <= -10){
+        number = -10
+        if(container[0].querySelector('#minimum-alert') === null){
+            const new_div = document.createElement('div')
+            new_div.innerHTML = '<h2> O número não pode ser menor do que -10 </h2>'
+            new_div.id = 'minimum-alert'
+            new_div.className = 'row justify-content-center mt-5'
+            container[0].appendChild(new_div)
+        }
+    }else{
+        if(container[0].querySelector('#maximum-alert') !== null){
+            container[0].querySelector('#maximum-alert').remove()
+            --number 
+        }else{
+            --number
+        }
+    }
+    value_element.textContent = number
+})
+
+increment_btn.addEventListener('click',(e)=>{
+    if(number >= 10){
+        number = 10
+        if(container[0].querySelector('#maximum-alert') === null){
+            const new_div = document.createElement('div')
+            new_div.innerHTML = '<h2> O número não pode ser maior do que 10 </h2>'
+            new_div.id = 'maximum-alert'
+            new_div.className = 'row justify-content-center mt-5'
+            container[0].appendChild(new_div)
+        }
+    }else{
+        if(container[0].querySelector('#minimum-alert') !== null){
+            container[0].querySelector('#minimum-alert').remove()
+            ++number 
+        }else{
+            ++number
+        }
+    }
+    value_element.textContent = number
+})
