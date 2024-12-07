6ad89d05e414b3020b0b2c4704b33a6d7f604adb
Igor <igor.torquato.20@gmail.com>
2024-12-07 19:16:28 -0300

    add: exercise5 solved

--- 11- Javascript DOM/exercicio_005/app.js
+++ 11- Javascript DOM/exercicio_005/app.js
@@ -6,3 +6,20 @@ Enunciado: TRÊS SLIDERS COM VALORES INDEPENDENTES - VERSÃO CÓDIGO REDUZIDO
     O exercício anterior, mas com quantidade de código JS reduzida.
 
 ---------------------------------------------------------------------------- */
+const range_1 = document.getElementById('range_1')
+const range_2 = document.getElementById('range_2')
+const range_3 = document.getElementById('range_3')
+
+const showValue = (range,val)=>{
+    range.setAttribute('min',0)
+    range.setAttribute('max',100)
+    range.value = 0
+
+    range.addEventListener('input',(event)=>{
+        document.getElementById(val).textContent = event.target.value
+    })
+}
+
+showValue(range_1,'value_1')
+showValue(range_2,'value_2')
+showValue(range_3,'value_3')
