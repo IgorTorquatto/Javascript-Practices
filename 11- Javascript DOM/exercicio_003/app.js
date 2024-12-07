d22fc26f03981ac731937e67931a97f39c2d7ccf
Igor <igor.torquato.20@gmail.com>
2024-12-07 17:15:36 -0300

    add: exercise3 solved

--- 11- Javascript DOM/exercicio_003/app.js
+++ 11- Javascript DOM/exercicio_003/app.js
@@ -10,3 +10,31 @@ Enunciado: CONTADOR COM CORES PARA NEGATIVOS, ZERO E POSITIVOS
     No caso do valor ser positivo o texto deve ser verde puro.
 
 ---------------------------------------------------------------------------- */
+const number_text = document.getElementById('valor')
+const de_btn = document.getElementById('btn_decremento')
+const in_btn = document.getElementById('btn_incremento')
+let number_value = parseInt(number_text.textContent)
+
+const checkColorValue = (num)=>{
+    if(num === 0){
+        number_text.style.color = '#ffffff'
+    }
+    else if(num < 0){
+        number_text.style.color = '#ff0000'
+    }
+    else{
+        number_text.style.color = '#00ff00'
+    }
+}
+
+de_btn.addEventListener('click',(e)=>{
+    --number_value
+    checkColorValue(number_value)
+    number_text.textContent = number_value
+})
+
+in_btn.addEventListener('click',(e)=>{
+    ++number_value
+    checkColorValue(number_value)
+    number_text.textContent = number_value
+})
